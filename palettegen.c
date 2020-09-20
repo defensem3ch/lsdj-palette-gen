d3b0b6a8535e7f5e4491d5d4972e5284cd00119c
urbster1 <urbster1@gmail.com>
2020-09-20 18:13:12 -0400

    simplify palettegen. thanks kotlinski

--- palettegen.c
+++ palettegen.c
@@ -3,23 +3,14 @@
 #include <stdint.h>
 #include <time.h>
 
-uint8_t r,g,b;
-short rgb[20];
-
 int main( int argc, char *argv[] ){
   if ( argc == 3 ) {
     srand(time(NULL));
     FILE* fpalette;
-    for (int i = 0; i < 20; i++) {
-      r = rand() % 32;
-      g = rand() % 32;
-      b = rand() % 32;
-      rgb[i] = (r << 10 | g << 5 | b);
-    }
     fpalette = fopen(argv[2], "wb+");
     fwrite(argv[1], sizeof(char), 4, fpalette);
-    for (int i = 0; i < 20; i++) {
-      fwrite(&rgb[i], sizeof(short), 1, fpalette);
+    for (int i = 0; i < 40; i++) {
+      fputc(rand(), fpalette);
     }
     fclose(fpalette);
   } else {
