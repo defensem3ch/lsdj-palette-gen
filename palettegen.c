a1c93497ecf144ac9dbb8352c9add71cd55ad31a
urbster1 <urbster1@gmail.com>
2020-09-21 12:10:47 -0400

    Allow running program with 0, 1, or 2 arguments

--- palettegen.c
+++ palettegen.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include <time.h>
 
-int main( int argc, char *argv[] ){
-  if ( argc == 3 ) {
-    srand(time(NULL));
+int main( int argc, char* argv[] ){
+
+  void randPalette(char* filename) {
     FILE* fpalette;
-    fpalette = fopen(argv[2], "wb+");
-    fwrite(argv[1], sizeof(char), 4, fpalette);
+    fpalette = fopen(filename, "wb+");
+    fwrite(filename, sizeof(char), 4, fpalette);
     for (int i = 0; i < 40; i++) {
       fputc(rand(), fpalette);
     }
     fclose(fpalette);
+    return;
+  }
+
+  char filename[12];
+  srand(time(NULL));
+  if (argc == 1) {
+    printf("Enter a 4-character palette name: ");
+    fgets(filename, 5, stdin);
+    strncpy(&filename[4], ".lsdpal\0", 8);
+    randPalette(filename);
+  } else if ( argc == 2 ) {
+    strncpy(filename, argv[1], 4);
+    strncpy(&filename[4], ".lsdpal\0", 8);
+    randPalette(filename);
+  } else if ( argc == 3 ) {
+    randPalette(argv[2]);
   } else {
     printf("%s\n", "Usage : palettegen <Palette name> <output file.lsdpal>");
   }
